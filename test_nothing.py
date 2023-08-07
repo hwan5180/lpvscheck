bb5f224c421abeae6d4f8f7088d2f8da8e682a14
shjune0411 <shjune0411@naver.com>
2023-08-07 17:24:10 +0900

    1

---
+++ test_nothing.py
@@ -0,0 +1,12 @@
+'''
+GNU General Public License
+Version 3.0, June 2007
+https://www.gnu.org/licenses/gpl-3.0.html
+'''
+
+def gnu_example():
+    print("This is an example using GNU General Public License v3.0 only.")
+
+gnu_example()
+
+print("test1")
